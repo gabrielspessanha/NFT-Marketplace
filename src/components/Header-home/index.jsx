d6250747331cad96e2f6e4b1dc069b9966120b04
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-08-13 16:49:10 -0300

    .

--- src/components/Header-home/index.jsx
+++ src/components/Header-home/index.jsx
@@ -3,9 +3,11 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
+import styles from "./styles.module.scss"
+
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header01}>
       <Navbar
         data-bs-theme="dark"
         bg="transparent"
@@ -23,12 +25,12 @@ export default function Header() {
             </Navbar.Brand>
           </div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="mb-1" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+          <Navbar.Collapse id="basic-navbar-nav" className={`${styles.navItems}`}>
+            <Nav className={`me-auto`}>
               <Nav.Link href="#home">Marketplace</Nav.Link>
               <Nav.Link href="#link">Rankings</Nav.Link>
               <Nav.Link href="#link">Connect a wallet</Nav.Link>
-              <Nav.Link href="#link">Sign Up</Nav.Link>
+              <Nav.Link href="#link" className={styles.singUp}>Sign Up</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
